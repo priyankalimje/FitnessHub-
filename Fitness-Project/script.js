1071986def065faf7bbeb6cab7066eaa8136c0ea
priyankalimje <149484807+priyankalimje@users.noreply.github.com>
2024-06-23 10:01:19 +0530

    Delete Fitness-Project directory

--- Fitness-Project/script.js
+++
@@ -1,7 +0,0 @@
-// Responsive navigation toggle
-const navToggle = document.querySelector('.nav-toggle');
-const navMenu = document.querySelector('.nav-menu');
-
-navToggle.addEventListener('click', () => {
-  navMenu.classList.toggle('nav-menu-visible');
-});
